b2a0487591aa0a875fe670d0f1041f1f0374e162
jaxels20 <xfm82gtn@gmail.com>
2022-06-28 10:48:51 +0200

    renamed and cleaned

---
+++ backend/src/server.go
@@ -0,0 +1,132 @@
+package src
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/pion/webrtc/v3"
+	"github.com/rs/xid"
+)
+
+type Client struct{
+	ID string
+	SocketConn *websocket.Conn
+	WebrtcConn *webrtc.PeerConnection
+}
+
+type Pool struct{
+	Clients map[*Client]bool
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Clients: make( map[*Client]bool),
+	}
+}
+
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func reader(client *Client, pool *Pool){
+
+	defer func(){
+		delete(pool.Clients, client)
+		client.SocketConn.Close()
+		fmt.Printf("client with id  %s has now left. %d users still connected \n", client.ID, len(pool.Clients))
+	}()
+
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+
+	var err error
+	client.WebrtcConn, err = webrtc.NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// event handler for icecandidates
+	client.WebrtcConn.OnICECandidate(
+		func(candidate *webrtc.ICECandidate) {
+			handleICECandidates(candidate, *client)
+		},
+	)
+	// event handler for opening a datachannel
+	client.WebrtcConn.OnDataChannel(
+		func(dc *webrtc.DataChannel) {
+			handleOpenDataChannel(dc, *client)
+		},
+	)
+
+	for {
+		messageType, p, err := client.SocketConn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// It is a offer:
+		if string(p)[9:14] == "offer"{
+			var offerMsg offerMsg	
+			json.Unmarshal(p, &offerMsg)
+			handleOffer(offerMsg, client.SocketConn, messageType, client.WebrtcConn)
+		}
+
+		//its a candidate:
+		if string(p)[9:18] == "candidate"{
+			var candidateMsg candidateMsg	
+			json.Unmarshal(p, &candidateMsg)
+
+			candidate := webrtc.ICECandidateInit{Candidate: candidateMsg.Payload.Candidate, SDPMid: candidateMsg.Payload.SdpMid, SDPMLineIndex: candidateMsg.Payload.SdpMLineIndex}
+			client.WebrtcConn.AddICECandidate(candidate)
+		}
+	}
+}
+
+
+func wsEndpoint(pool *Pool, w http.ResponseWriter, r *http.Request){
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true}
+
+	client := Client{
+		ID: xid.New().String(),
+	}
+
+	pool.Clients[&client] = true
+
+	var err error
+	client.SocketConn, err = upgrader.Upgrade(w,r, nil)
+	if err != nil{
+		log.Println(err)
+	}
+
+	log.Println("client succesfully connected to the server")
+	go reader(&client, pool)
+}
+
+
+func setupRoutes() {
+
+	pool := NewPool()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		wsEndpoint(pool, w, r)
+	})
+
+}
+
+func Run() {
+	fmt.Println("Starting server")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
