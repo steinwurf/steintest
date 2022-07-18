66555359a72350eb09982a3e20677166122d4500
jaxels20 <xfm82gtn@gmail.com>
2022-07-18 12:12:17 +0200

    fixed echoserver main

--- echoserver/main.go
+++ echoserver/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	//"SteinTest/src"
+	"SteinTest/src"
 
 )
 
 
 func main() {
-	//src.Run()
-
-	jsonstring := {"id":354,│
-"sentAt":1657280492693,"recv":true,"recvAt":1657280492849,"delay":156,"delayed":false},{"id":355,"sen│
-tAt":1657280492693,"recv":true,"recvAt":1657280492849,"delay":156,"delayed":false},{"id":356,"sentAt"│
-:1657280492693,"recv":true,"recvAt":1657280492849,"delay":156,"delayed":false},{"id":357,"sentAt":165│
-7280492693,"recv":true,"recvAt":1657280492849,"delay":156,"delayed":false},{"id":358,"sentAt":1657280│
-492693,"recv":true,"recvAt":1657280492849,"delay":156,"delayed":false},{"id":359,"sentAt":16572804926│
-93,"recv":true,"recvAt":1657280492849,"delay":156,"delayed":false},{"id":360,"sentAt":1657280492693,"│
-recv":true,"recvAt":1657280492849,"delay":156,"delayed":false}
+	src.Run()
 }
