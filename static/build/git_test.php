44806d49a6e8cc846e04c065cf3201100885f093
jaxels20 <xfm82gtn@gmail.com>
2022-08-01 11:01:42 +0200

    deleted the old website and added scripts for automatic realeses

---
+++ static/build/git_test.php
@@ -0,0 +1,19 @@
+<?php 
+try
+{
+  $payload = json_decode($_REQUEST['payload']);
+}
+catch(Exception $e)
+{
+  exit(0);
+}
+
+//log the request
+file_put_contents('github_build_logs.txt', print_r($payload, TRUE), FILE_APPEND);
+
+
+/* if ($payload->ref === 'refs/heads/master')
+{
+  // path to your site deployment script
+  exec('./build.sh');
+} */
