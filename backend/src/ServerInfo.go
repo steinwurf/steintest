78d2cd9c6e347d6f32ed1cd8a378e1df6eed34a7
jaxels20 <xfm82gtn@gmail.com>
2022-08-09 10:23:43 +0200

    changed gitignore

--- backend/src/ServerInfo.go
+++
@@ -1,9 +0,0 @@
-package src 
-
-const(
-	
-	dbconnectionstring = "mongodb+srv://NY-server:6mc5Y0lVj2z1934K@db-mongo-81099e05.mongo.ondigitalocean.com/steintest?tls=true&authSource=admin&replicaSet=db-mongo"
-	collectionName = "sessiondata"
-	dataBaseName = "steintest"
-	ServerName = "New York"
-)
