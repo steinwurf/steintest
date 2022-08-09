ed0c2d6e7bcf623a4ffe4dca4f5fe8d513f47aeb
Your Name <you@example.com>
2022-08-09 08:16:19 +0000

    test

--- backend/src/ServerInfo.go
+++ backend/src/ServerInfo.go
@@ -5,6 +5,6 @@ const(
 	dbconnectionstring = "mongodb+srv://NY-server:6mc5Y0lVj2z1934K@db-mongo-81099e05.mongo.ondigitalocean.com/steintest?tls=true&authSource=admin&replicaSet=db-mongo"
 	collectionName = "sessiondata"
 	dataBaseName = "steintest"
-	ServerName = "New York"
+	ServerName = "Singapore"
 
 )
