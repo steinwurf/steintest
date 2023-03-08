afcb0caa2abe7162c1f311d69615282f5df02a50
jaxels20 <xfm82gtn@gmail.com>
2023-03-08 10:54:19 +0100

    testing the workflow

---
+++ tests/test_integration.py
@@ -0,0 +1,29 @@
+import pytest 
+import os
+import sys
+import tests.utils as utils
+sys.path.append("..")
+import demo.run_demo as f
+
+# parametrize the test
+@pytest.mark.parametrize("expected_packet_loss", [0, 5, 10, 20, 50])
+@pytest.mark.parametrize("duration", [5, 10])
+@pytest.mark.parametrize("frequency", [100, 200, 300])
+@pytest.mark.parametrize("packet_size", [100])
+def test_integration(expected_packet_loss, duration, frequency, packet_size):
+    os.chdir(str(utils.demo_path))
+
+    f.run_full_demo(expected_packet_loss, duration, frequency, packet_size)
+
+    # get the test result
+
+    packet_loss = utils.get_loss_from_last_test()
+
+    # calculate the bounds
+    lower_bound, upper_bound = utils.calculate_packetloss_bounds(expected_packet_loss, duration, frequency)
+
+    assert expected_packet_loss - lower_bound <= packet_loss <= expected_packet_loss + upper_bound
+
+
+
+
---
+++ tests/utils.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from pymongo import MongoClient
+from datetime import datetime
+import pandas as pd
+from math import sqrt
+import time
+import logging
+
+
+demo_path = Path(__file__).resolve().parents[1] / "demo"
+connection_string_path = demo_path / "connection_string.txt"
+def load_connection_string():
+    with open(connection_string_path, 'r') as f:
+        return f.read()
+    
+
+def get_loss_from_last_test():
+    client = MongoClient(load_connection_string())
+    db = client['test']
+    col = db["default"]
+
+    # retrieve the last inserted document
+    last_doc = col.find().sort([('_id', -1)]).limit(1)[0]
+
+    raw_data = last_doc["raw_data"]
+    epoch = last_doc["meta_data"]["epoch"]
+    time = datetime.fromtimestamp(epoch/1000)
+
+    # create df from the last inserted document
+
+    df = pd.DataFrame(raw_data)
+
+    # the number of received packets
+    received_packets = len(df[df["received"] == True])
+    dropped_packets = len(df[df["received"] == False])
+
+    # calculate the packet loss
+    packet_loss = dropped_packets / (received_packets + dropped_packets) * 100
+
+    return packet_loss
+
+def calculate_packetloss_bounds(expected_packet_loss, duration, frequency):
+
+    # calculate the number of packets sent
+    packets_sent = duration * frequency
+    expected_packet_loss_decimal = expected_packet_loss / 100
+
+    mean = expected_packet_loss_decimal
+    std = sqrt(expected_packet_loss_decimal * (1 - expected_packet_loss_decimal) / packets_sent)
+
+    # calculate the 99 % bounds
+    lower_bound = mean - 2.58 * std
+    upper_bound = mean + 2.58 * std
+
+    return  lower_bound * 100, upper_bound * 100
