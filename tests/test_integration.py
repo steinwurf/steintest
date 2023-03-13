b4a9102ba7e4a25c09310155ae68bdfe85bc302d
jaxels20 <xfm82gtn@gmail.com>
2023-03-13 11:00:42 +0100

    test workflow

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,8 +4,10 @@ import sys
 import tests.utils as utils
 sys.path.append("..")
 import demo.run_demo as f
+import subprocess
+import time
 
-# parametrize the test
+""" # parametrize the test
 @pytest.mark.parametrize("expected_packet_loss", [2])
 @pytest.mark.parametrize("duration", [5])
 @pytest.mark.parametrize("frequency", [100])
@@ -22,8 +24,37 @@ def test_integration(expected_packet_loss, duration, frequency, packet_size):
     # calculate the bounds
     lower_bound, upper_bound = utils.calculate_packetloss_bounds(expected_packet_loss, duration, frequency)
 
+    assert expected_packet_loss - lower_bound <= packet_loss <= expected_packet_loss + upper_bound """
+
+@pytest.mark.parametrize("expected_packet_loss", [2])
+@pytest.mark.parametrize("duration", [5])
+@pytest.mark.parametrize("frequency", [100])
+@pytest.mark.parametrize("packet_size", [100])
+def test1_integration(expected_packet_loss, duration, frequency, packet_size):
+    os.chdir(str(utils.demo_path))
+
+    # run the server 
+    server = subprocess.Popen(["sudo ip netns exec ns1 ./server", "-serverParams server_params.json"], shell=True)
+
+    # run the client
+    client = subprocess.Popen(["sudo ip netns exec ns2 ./client", "-clientParams"], shell=True)
+
+    # wait 1 minute
+    time.sleep(60)
+
+    # kill the server
+    server.kill()
+
+    # kill the client
+    client.kill()
+
+    # get the test result
+
+    packet_loss = utils.get_loss_from_last_test()
+
+    # calculate the bounds
+    lower_bound, upper_bound = utils.calculate_packetloss_bounds(expected_packet_loss, duration, frequency)
+
     assert expected_packet_loss - lower_bound <= packet_loss <= expected_packet_loss + upper_bound
-
-
 
 
